53d0be7ba760652d0327e25b316f89d4a840482e
deepakness <deepakkr342@gmail.com>
2023-03-01 14:23:08 +0530

    added dall-e calc

---
+++ dall-e/dalleScript.js
@@ -0,0 +1,17 @@
+function calculatePrice() {
+    // Get the input and multiplier values
+    const input = document.getElementById("input").value;
+    const multiplier = document.querySelector('input[name="multiplier"]:checked').value;
+  
+    // Calculate the price
+    const price = input * multiplier;
+  
+    // Update the output field
+    const output = document.getElementById("price");
+    output.textContent = price.toFixed(3);
+
+    // Update ##
+    const words = document.getElementById("images");
+    words.textContent = input;
+  }
+  
