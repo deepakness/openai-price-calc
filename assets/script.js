ca986d416bf1e5a2eb6c3ea4d7a56dcf81c52b3e
deepakness <deepakkr342@gmail.com>
2023-03-01 08:15:27 +0530

    init

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+function calculatePrice() {
+    // Get the input and multiplier values
+    const input = document.getElementById("input").value;
+    const multiplier = document.querySelector('input[name="multiplier"]:checked').value;
+  
+    // Calculate the price
+    const price = input / 750 * multiplier;
+  
+    // Update the output field
+    const output = document.getElementById("price");
+    output.textContent = price.toFixed(4);
+
+    // Update ##
+    const words = document.getElementById("words");
+    words.textContent = input;
+  }
+  
