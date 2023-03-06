a0aeda2d6d1acb63bc8361d3b8b8f8331fc1af98
deepakness <deepakkr342@gmail.com>
2023-03-06 09:37:12 +0530

    added prompt length for calc.

--- gptScript.js
+++ gptScript.js
@@ -1,10 +1,11 @@
 function calculatePrice() {
     // Get the input and multiplier values
     const input = document.getElementById("input").value;
+    const prompt = document.getElementById("prompt").value;
     const multiplier = document.querySelector('input[name="multiplier"]:checked').value;
   
     // Calculate the price
-    const price = input / 750 * multiplier;
+    const price = (input / 750 * multiplier) + ((input / 1000) * (prompt / 750) * multiplier);
   
     // Update the output field
     const output = document.getElementById("price");
