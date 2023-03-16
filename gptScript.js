1a40968812bc8709fc372a59c3c268cb19c9c795
deepakness <deepakkr342@gmail.com>
2023-03-16 06:29:55 +0530

    added gpt-4

--- gptScript.js
+++ gptScript.js
@@ -3,9 +3,17 @@ function calculatePrice() {
     const input = document.getElementById("input").value;
     const prompt = document.getElementById("prompt").value;
     const multiplier = document.querySelector('input[name="multiplier"]:checked').value;
+    const checkMultiplier = document.querySelector('input[name="multiplier"]:checked').id;
   
     // Calculate the price
-    const price = (input / 750 * multiplier) + ((input / 1000) * (prompt / 750) * multiplier);
+    let price;
+    if (checkMultiplier == multiplier1) {
+      price = (input / 750 * multiplier) + ((input / 1000) * (prompt / 750) * 0.03);
+    } else if (checkMultiplier == multiplier2) {
+      price = (input / 750 * multiplier) + ((input / 1000) * (prompt / 750) * 0.06);
+    } else {
+      price = (input / 750 * multiplier) + ((input / 1000) * (prompt / 750) * multiplier);
+    }
   
     // Update the output field
     const output = document.getElementById("price");
