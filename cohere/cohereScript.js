cce4485701f0e4ebed03dede1a3e35833e7c7f95
deepakness <deepakkr342@gmail.com>
2023-05-25 11:47:17 +0530

    added cohere calc

---
+++ cohere/cohereScript.js
@@ -0,0 +1,62 @@
+function calculatePrice() {
+    // Get the input and multiplier values
+    const input = document.getElementById("input").value;
+    const multiplier = document.querySelector('input[name="multiplier"]:checked').value;
+    const checkMultiplier = document.querySelector('input[name="multiplier"]:checked').id;
+  
+    // Calculate the price
+    const inputPriceThousandTokens = multiplier * 1000 / 1000000;
+    let price;
+    if (checkMultiplier == "multiplier1") {
+        price = input * multiplier;
+    }
+    else if (checkMultiplier == "multiplier2") {
+        price = input * multiplier;
+    }
+    else if (checkMultiplier == "multiplier3") {
+        price = input * multiplier;
+    }
+    else if (checkMultiplier == "multiplier4") {
+        price = input * multiplier;
+    }
+    else if (checkMultiplier == "multiplier5") {
+        price = input * multiplier;
+    }
+    else {
+        price = "Error!";
+    }
+  
+    // Update the label text
+    let labelText;
+    if (checkMultiplier == "multiplier1") {
+        labelText = "embeddings";
+    }
+    else if (checkMultiplier == "multiplier2") {
+        labelText = "generation units";
+    }
+    else if (checkMultiplier == "multiplier3") {
+        labelText = "classifications";
+    }
+    else if (checkMultiplier == "multiplier4") {
+        labelText = "summarization units";
+    }
+    else if (checkMultiplier == "multiplier5") {
+        labelText = "search units";
+    }
+
+    const updateLabel = document.getElementById("updateLabel");
+    updateLabel.textContent = labelText;
+
+    // Update units
+    const updateUnits = document.getElementById("updateUnits");
+    updateUnits.textContent = labelText;
+
+    // Update the output field
+    const output = document.getElementById("price");
+    output.textContent = "$" + price.toFixed(4);
+
+    // Update words
+    const words = document.getElementById("words");
+    words.textContent = input;
+  }
+  
